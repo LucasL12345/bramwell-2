f60041ca33c1038792a29860567a8dacc7c4aa91
LucasL12345 <lucaslemoine49@gmail.com>
2026-01-14 22:17:56 +0000

    Version 2

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
 import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Navbar } from "@/components/layout/Navbar";
@@ -28,18 +26,16 @@ function Router() {
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <div className="flex flex-col min-h-screen font-body text-slate-900">
-          <Navbar />
-          <main className="flex-grow">
-            <Router />
-          </main>
-          <Footer />
-        </div>
-        <Toaster />
-      </TooltipProvider>
-    </QueryClientProvider>
+    <TooltipProvider>
+      <div className="flex flex-col min-h-screen font-body text-slate-900">
+        <Navbar />
+        <main className="flex-grow">
+          <Router />
+        </main>
+        <Footer />
+      </div>
+      <Toaster />
+    </TooltipProvider>
   );
 }
 
